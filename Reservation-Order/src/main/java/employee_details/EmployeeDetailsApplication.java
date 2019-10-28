9ebeb7a395fb1b1721d9481b29903c036eb8457f
zhaneka <zhannaorazova85@gmail.com>
2019-10-28 15:24:30 +0600

    hystrix

--- Reservation-Order/src/main/java/employee_details/EmployeeDetailsApplication.java
+++ Reservation-Order/src/main/java/employee_details/EmployeeDetailsApplication.java
@@ -2,11 +2,12 @@ package employee_details;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableCircuitBreaker
 public class EmployeeDetailsApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeDetailsApplication.class, args);
